36b7d15a483012709466d6b750e8c34cf82ba4cc
fan.li <fan.li@corp.elong.com>
2016-11-02 12:16:03 +0800

    ...

--- src/async-list.js
+++ src/async-list.js
@@ -4,15 +4,19 @@ export default function(list){
 		var count = list.length,
 			results = [];
 		if(list && count > 0){
-			list.forEach((item, index) => {
-				item(result => {
-					results[index] = result;
-					count --;
-					if(count === 0){
-						resolve(...results);
-					}
+			try{
+				list.forEach((item, index) => {
+					item(result => {
+						results[index] = result;
+						count --;
+						if(count === 0){
+							resolve(...results);
+						}
+					});
 				});
-			});
+			}catch(e){
+				reject(e);
+			}
 		}else{
 			resolve();
 		}
