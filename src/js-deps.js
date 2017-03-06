d04f1ea04c2a8ee4c6017beec0aa389a06657910
李帆 <fan.li@corp.elong.com>
2017-03-06 15:30:20 +0800

    jsDeps.replace

--- src/js-deps.js
+++ src/js-deps.js
@@ -61,6 +61,31 @@ jsDeps.replace = function(content, fn){
 			sourceMaps: process.env.SM ? "inline" : false,
 			plugins: [
 				function ({ types: t }) {
+					// 根据modId的类型，生成相应的节点
+					function createModId (modId) {
+						switch(typeof modId){
+							case "string":
+								return t.StringLiteral(modId);
+							case "number":
+								return t.NumericLiteral(modId);
+						}
+						throw new Error("modId'" + modId + "'不是有效的类型");
+					}
+					// 向节点添加注释
+					function addComments (node, comments) {
+						if(!comments.trim()){
+							return;
+						}
+
+						if(!node.trailingComments){
+							node.trailingComments = [];
+						}
+
+						comments.split("\n").forEach(function(comment){
+							node.trailingComments.push(t.StringLiteral(comment));
+						});
+					}
+
 					return {
 						visitor: {
 							CallExpression: {
@@ -77,29 +102,33 @@ jsDeps.replace = function(content, fn){
 											if(!modInfo){
 												return;
 											}
+
+											if(!modInfo.modId){
+												path.node = t.NullLiteral();
+												return;
+											}
+
+											if(modInfo instanceof Array){
+												path.node = t.ArrayExpression(modInfo.map(function(modInfo){
+													var modId = createModId(modInfo.modId);
+													addComments(modId, modInfo.comments);
+
+													return t.CallExpression(
+															t.Identifier(modInfo.requireName || "require"),
+															modId
+														);
+												}));
+												return;
+											}
 											
 											// 修改require的模块ID
-											if(modInfo.modId){
-												switch(typeof modInfo.modId){
-													case "string":
-														node.arguments[0] = t.StringLiteral(modInfo.modId);
-														break;
-													case "number":
-														node.arguments[0] = t.NumericLiteral(modInfo.modId);
-														break;
-												}
-											}
+											node.arguments[0] = createModId(modInfo.modId);
 											// 修改require的方法名
 											if(modInfo.requireName){
 												node.callee.name = modInfo.requireName;
 											}
 											// 添加注释
-											if(modInfo.comments){
-												if(!node.arguments[0].trailingComments){
-													node.arguments[0].trailingComments = [];
-												}
-												node.arguments[0].trailingComments.push(t.StringLiteral(modInfo.comments));
-											}
+											addComments(node.arguments[0], modInfo.comments);
 									}
 								}
 							}
